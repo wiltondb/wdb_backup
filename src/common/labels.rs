55c5a6437bac97ce7a4ce3ddd32db60241e02baf
alex <alex@staticlibs.net>
2024-04-13 07:44:28 +0100

    do not assume that pg_dump output is valid utf-8

--- src/common/labels.rs
+++ src/common/labels.rs
@@ -14,4 +14,4 @@
  * limitations under the License.
  */
 
-pub static VERSION: &str = "1.0.3";
+pub static VERSION: &str = "1.0.4";
--- src/restore_dialog/dialog.rs
+++ src/restore_dialog/dialog.rs
@@ -219,12 +219,22 @@ impl RestoreDialog {
             Err(e) => return Err(io::Error::new(io::ErrorKind::Other, format!(
                 "pg_restore process spawn failure: {}", e)))
         };
-        for line in BufReader::new(&reader).lines() {
-            match line {
-                Ok(ln) => progress.send_value(ln),
+        let mut buf_reader = BufReader::new(&reader);
+        loop {
+            let mut buf = vec!();
+            match buf_reader.read_until(b'\n', &mut buf) {
+                Ok(len) => {
+                    if 0 == len {
+                        break;
+                    }
+                    if buf.len() >= 2 {
+                        let ln = String::from_utf8_lossy(&buf[0..buf.len() - 2]);
+                        progress.send_value(ln);
+                    }
+                },
                 Err(e) => return Err(io::Error::new(io::ErrorKind::Other, format!(
                     "pg_restore process failure: {}", e)))
-            }
+            };
         };
         match reader.try_wait() {
             Ok(opt) => match opt {
